40499b8b3f86697df432cbda35e7e09c4df13c1e
jaque20050 <jaque20050@gmail.com>
2022-01-27 11:11:52 -0300

    Estudo com Java/ Carros

---
+++ Carros/src/Carro.java
@@ -0,0 +1,25 @@
+
+public class Carro {
+
+	int potencia;
+
+	int velocidade;
+
+	String nome;
+
+	void acelerar() {// Metodo
+		velocidade += potencia;
+	}
+
+	void frear() {
+		velocidade = velocidade / 2;
+	}
+
+	int getVelocidade() {
+		return velocidade;
+	}
+
+	void imprimir() {
+		System.out.println("O carro " + nome + " esta a velocidade " + getVelocidade() + " Km/h");
+	}
+}
